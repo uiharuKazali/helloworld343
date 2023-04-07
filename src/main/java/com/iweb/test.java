a1ad139c5eedf458c2ad3b76b819a4939b8368e2
xbl <2936412321@qq.com>
2023-04-07 15:04:23 +0800

    代码在每一次提交的时候，都需要提供提交信息，如果是多人协同项，你需要通过提交信息告诉比尔 你提交了什么diamante 实现了什么功能 或者解决了哪些问题

---
+++ src/main/java/com/iweb/test.java
@@ -0,0 +1,11 @@
+package com.iweb;
+
+/**
+ * @author by xbl
+ * @date 2023/4/7 14:55
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
